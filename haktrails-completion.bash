be940404ab88332201f5776c59e781030a5ec698
Albony <albonycal@gmail.com>
2021-03-29 15:52:18 +0530

    zsh and bash autocompletion

---
+++ haktrails-completion.bash
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+complete -W "\subdomains company details tags whois ping banner associatedips associateddomains historicaldns historicalwhois" haktrails
---
+++ haktrails-completion.zsh
@@ -0,0 +1,5 @@
+compdef _haktrails haktrails
+
+function _haktrails {
+    _arguments "1: :(subdomains company details tags whois ping banner associatedips associateddomains historicaldns historicalwhois)"
+}
